1f2224f8a4f5230e32002d0e40336f33b542992b
Pantelis Roditis <proditis@echothrust.com>
2015-11-16 10:33:05 +0200

    Make sure we allow to enter IP's that are more than 10 chars.
    Issue #22

--- htdocs/protected/modules/settings/views/host/_search.php
+++ htdocs/protected/modules/settings/views/host/_search.php
@@ -5,7 +5,7 @@
 
 		<?php echo $form->textFieldRow($model,'id',array('class'=>'span5','maxlength'=>10)); ?>
 		
-		<?php echo $form->textFieldRow($model,'ip',array('class'=>'span5','maxlength'=>10)); ?>
+		<?php echo $form->textFieldRow($model,'ip',array('class'=>'span5','maxlength'=>16)); ?>
 		
 		<?php echo $form->textFieldRow($model,'fqdn',array('class'=>'span5','maxlength'=>255)); ?>
 
