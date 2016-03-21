8c45c97d6e949b96dbfbcf9ba9d3601750b41d8f
Pantelis Roditis <proditis@echothrust.com>
2016-03-21 15:20:58 +0200

    Introduce addFlash will be needed to include multiple messages from the system

--- htdocs/protected/components/WebUser.php
+++ htdocs/protected/components/WebUser.php
@@ -1,33 +1,43 @@
 <?php
 class WebUser extends CWebUser {
-  // Store model to not repeat query.
-  private $_model;
- 
-  // Load user model.
-  function getUser_Model(){
-    $user=$this->loadUser($this->id);
-    return $user;
-  }
-  public function isAdmin()
-  {
-    $user=$this->loadUser($this->id);
-    return $user && $user->superuser==1;
-  }
+	// Store model to not repeat query.
+	private $_model;
+
+	// Load user model.
+	function getUser_Model(){
+		$user=$this->loadUser($this->id);
+		return $user;
+	}
+
+	public function isAdmin()
+	{
+		$user=$this->loadUser($this->id);
+		return $user && $user->superuser==1;
+	}
 
 	public function getIsAdmin()
 	{
 		return $this->isAdmin();
 	}
-	
-  protected function loadUser($id=null)
-    {
-        if($this->_model===null)
-        {
-            if($id!==null)
-                $this->_model=User::model()->findByPk($id);
-            else return null;
-        }
-        return $this->_model;
-    }
+
+	protected function loadUser($id=null)
+	{
+		if($this->_model===null)
+		{
+			if($id!==null)
+				$this->_model=User::model()->findByPk($id);
+			else return null;
+		}
+		return $this->_model;
+	}
+
+	public function addFlash($key,$value) {
+		if($this->hasFlash($key)) {
+			$actualVal = $this->getFlash($key);
+			$array = is_array($actualVal) ? $actualVal : array($actualVal);
+			$array[] = $value;
+			$this->setFlash($key, $array);
+		} else $this->setFlash($key, $value);
+	}
 }
 ?>
