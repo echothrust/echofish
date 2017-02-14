af31573fc24782f86af62c95a31eee838f47275f
Pantelis Roditis <proditis@echothrust.com>
2017-02-14 11:39:22 +0200

    Previus attempt didnt seem to bring any results back.

--- htdocs/protected/modules/syslog/models/Log.php
+++ htdocs/protected/modules/syslog/models/Log.php
@@ -108,7 +108,7 @@ Abstract class Log extends CActiveRecord
 		$criteria->compare('id',$this->id,true);
 		if($this->fromTS!==null && $this->toTS!==null)
 		{
-			$criteria->addBetweenCondition('received_ts',"FROM_UNIXTIME(".$this->fromTS."/1000)","FROM_UNIXTIME(".$this->toTS."/1000)");
+			$criteria->addBetweenCondition('UNIX_TIMESATMP(received_ts)*1000',$this->fromTS,$this->toTS);
 		}
 		else
 			$criteria->compare('received_ts',$this->received_ts,true);
