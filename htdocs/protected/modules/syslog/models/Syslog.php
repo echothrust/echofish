cca9938efa8db4d7b1e9dd6899ae21d0dd89c96a
Pantelis Roditis <proditis@echothrust.com>
2017-02-20 13:02:15 +0200

    Bring back the massack operations for host based filters and make sure we display error messages back to the user

--- htdocs/protected/modules/syslog/models/Syslog.php
+++ htdocs/protected/modules/syslog/models/Syslog.php
@@ -43,7 +43,7 @@ class Syslog extends Log
 	public function acknowledge_logs($criteria=null)
 	{
 		if($criteria->condition!="")
-			$cmd = Yii::app()->db->createCommand("DELETE t1.* FROM syslog as t1 LEFT JOIN host as t2 ON t2.id=t1.host WHERE ".$criteria->condition);
+			$cmd = Yii::app()->db->createCommand("DELETE t1.* FROM syslog as t1 LEFT JOIN host ON host.id=t1.host WHERE ".$criteria->condition);
 		else
 			$cmd = Yii::app()->db->createCommand("DELETE t1.* FROM syslog as t1");
 		foreach($criteria->params as $key=>$val) $cmd->bindParam($key,$val);
